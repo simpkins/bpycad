70b7714a3d716a63c28557a7571a4cccbdc24463
Adam Simpkins <adam@adamsimpkins.net>
2023-08-11 11:29:13 -0700

    Improve tracking of modules to monitor
    Replace sys.meta_path with our own import tracker while we are running
    the function.  This allows us to track all attempted imports, even if
    the module does not exist or if an exception occurs trying to import it.
    This lets us correctly monitor all desired paths even after an error.

--- bpycad/auto_update.py
+++ bpycad/auto_update.py
@@ -19,11 +19,103 @@ import bpy
 import importlib
 import sys
 import traceback
+from importlib.abc import MetaPathFinder
+from importlib.machinery import ModuleSpec
 from pathlib import Path
-from typing import Dict, List, Optional, Set, Tuple
+from types import ModuleType, TracebackType
+from typing import Dict, List, Optional, Sequence, Set, Tuple, Type
 
 
 _instance: Optional[MonitorOperatorBase] = None
+
+
+class ImportTracker(MetaPathFinder):
+    """
+    A helper class to monitor imports that happen, so we can keep track
+    of which modules we need to monitor.
+
+    This hooks into the import system so we can record modules we attempt to
+    import even if an error occurs while importing them.
+    """
+
+    def __init__(self) -> None:
+        self.real_meta_path: Optional[List[MetaPathFinder]] = None
+        self.imported_modules: Dict[str, List[Path]] = {}
+
+    def __enter__(self) -> ImportTracker:
+        self.install()
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        self.uninstall()
+
+    def install(self) -> None:
+        assert not self.real_meta_path
+        # pyre-ignore[8]: typeshed defines sys.meta_path using a protocol
+        #   rather than MetaPathFinder, and has annoying inconsistencies
+        #   between its protocol and the real type.
+        self.real_meta_path = sys.meta_path
+        sys.meta_path = [self]
+
+    def uninstall(self) -> None:
+        meta_path = self.real_meta_path
+        assert meta_path is not None
+        # pyre-ignore[9]: same typeshed sys.meta_path problems as above.
+        sys.meta_path = meta_path
+        self.real_meta_path = None
+
+    # pyre-fixme[14]: pyre reports an inconsistent override here when using
+    #  slightly out-of-date typeshed hints from before
+    #  https://github.com/python/typeshed/pull/9070
+    def find_spec(
+        self,
+        fullname: str,
+        path: Optional[Sequence[str]],
+        target: Optional[ModuleType] = None,
+    ) -> Optional[ModuleSpec]:
+        meta_path = self.real_meta_path
+        assert meta_path is not None
+        for mpf in meta_path:
+            spec = mpf.find_spec(fullname, path, target=target)
+            if spec is not None:
+                paths: List[Path] = []
+                if spec.origin is not None:
+                    paths.append(Path(spec.origin))
+                self.imported_modules[fullname] = paths
+                return spec
+
+        # If we reach here we failed to find this module.
+        # Even though we could not find it, monitor the path were it is likely
+        # to be.  We want to immediately reload if this file does get created
+        # in the future.
+        if path:
+            search_path = path
+        else:
+            # If this is a top-level, walk through sys.path and add possible
+            # locations where it might not be found.  This might not be 100%
+            # accurate with what the real MetaPathFinders would do, but is good
+            # enough for most cases.
+            search_path = sys.path
+        module_base = fullname.rsplit(".", 1)[-1]
+        paths: List[Path] = []
+        for p in search_path:
+            paths.append(Path(p) / f"{module_base}.py")
+            paths.append(Path(p) / module_base / "__init__.py")
+        print(f"unable to find import: {fullname} ; will search {paths}")
+        self.imported_modules[fullname] = paths
+
+        return None
+
+    def invalidate_caches(self) -> None:
+        meta_path = self.real_meta_path
+        assert meta_path is not None
+        for mpf in meta_path:
+            mpf.invalidate_caches()
 
 
 class MonitorOperatorBase(bpy.types.Operator):
@@ -48,8 +140,7 @@ class MonitorOperatorBase(bpy.types.Operator):
     _timer: Optional[bpy.types.Timer] = None
     stop: bool = False
 
-    _monitored_paths: List[Path] = []
-    _monitored_modules: List[str] = []
+    _monitored_modules: Dict[str, List[Path]] = {}
     _timestamps: Dict[Path, Optional[float]] = {}
     _name: str = ""
 
@@ -84,7 +175,9 @@ class MonitorOperatorBase(bpy.types.Operator):
         self._monitored_packages = self.monitored_packages.split(",")
 
         self._init()
-        self.on_change(force_refresh_module_list=True)
+        self.report({"INFO"}, f"monitoring modules for {self._name}")
+        self._init_monitored_modules()
+        self.on_change()
 
         wm = context.window_manager
         self._timer = wm.event_timer_add(
@@ -93,30 +186,36 @@ class MonitorOperatorBase(bpy.types.Operator):
         wm.modal_handler_add(self)
         return {"RUNNING_MODAL"}
 
-    def _refresh_monitored_modules(self) -> None:
-        self._monitored_modules, self._monitored_paths = (
-            self._init_monitor_modules()
-        )
+    def _refresh_monitored_modules(
+        self, import_tracker: ImportTracker
+    ) -> None:
+        monitored_modules: Dict[str, List[Path]] = {}
+        for mod_name, paths in import_tracker.imported_modules.items():
+            paths_to_monitor = self._get_monitor_paths(mod_name, paths)
+            if paths_to_monitor:
+                monitored_modules[mod_name] = paths_to_monitor
 
-        self.report({"INFO"}, f"monitoring modules for {self._name}")
+        self._monitored_modules = monitored_modules
         self._timestamps = self._get_timestamps()
 
-    def _init_monitor_modules(self) -> Tuple[List[str], List[Path]]:
-        monitored_modules: List[str] = []
-        monitored_paths: List[Path] = []
-
+    def _init_monitored_modules(self) -> None:
         for mod_name, module in sys.modules.items():
-            mod_path = getattr(module, "__file__", None)
-            if mod_path is None:
-                continue
+            mod_path_str = getattr(module, "__file__", None)
+            mod_paths: List[Path] = []
+            if mod_path_str is not None:
+                mod_paths.append(Path(mod_path_str))
+            paths_to_monitor = self._get_monitor_paths(mod_name, mod_paths)
+            if paths_to_monitor:
+                self._monitored_modules[mod_name] = paths_to_monitor
 
-            for pkg in self._monitored_packages:
-                if mod_name == pkg or mod_name.startswith(pkg + "."):
-                    monitored_modules.append(mod_name)
-                    monitored_paths.append(Path(mod_path))
-                    break
+    def _get_monitor_paths(
+        self, mod_name: str, mod_paths: List[Path]
+    ) -> List[Path]:
+        for pkg in self._monitored_packages:
+            if mod_name == pkg or mod_name.startswith(pkg + "."):
+                return mod_paths
 
-        return monitored_modules, monitored_paths
+        return []
 
     def cancel(self, context: bpy.types.Context) -> None:
         wm = context.window_manager
@@ -155,8 +254,9 @@ class MonitorOperatorBase(bpy.types.Operator):
 
     def _get_timestamps(self) -> Dict[Path, Optional[float]]:
         result: Dict[Path, Optional[float]] = {}
-        for path in self._monitored_paths:
-            result[path] = self._get_timestamp(path)
+        for path_list in self._monitored_modules.values():
+            for path in path_list:
+                result[path] = self._get_timestamp(path)
         return result
 
     def _get_timestamp(self, path: Path) -> Optional[float]:
@@ -166,7 +266,7 @@ class MonitorOperatorBase(bpy.types.Operator):
             return None
         return s.st_mtime
 
-    def on_change(self, force_refresh_module_list: bool = False) -> None:
+    def on_change(self) -> None:
         if self.delete_all:
             if bpy.context.object is not None:
                 bpy.ops.object.mode_set(mode="OBJECT")
@@ -178,6 +278,8 @@ class MonitorOperatorBase(bpy.types.Operator):
         self.report({"INFO"}, f"running {self._name}")
 
         error = False
+        import_tracker = ImportTracker()
+        import_tracker.install()
         try:
             self._run()
             print(f"Finished {self._name}")
@@ -190,10 +292,10 @@ class MonitorOperatorBase(bpy.types.Operator):
             # full new file.
             self._report_error(f"error running {self._name}")
             error = True
+        finally:
+            import_tracker.uninstall()
 
-        if force_refresh_module_list or not error:
-            # Only refresh the list of monitored modules after a successful run.
-            self._refresh_monitored_modules()
+        self._refresh_monitored_modules(import_tracker)
 
     def _init(self) -> None:
         pass
@@ -203,7 +305,11 @@ class MonitorOperatorBase(bpy.types.Operator):
 
     def _report_error(self, msg: str) -> None:
         err_str = traceback.format_exc()
-        self.report({"INFO"}, f"{msg}: {err_str}")
+        # I haven't quite tracked down why, but Blender doesn't always write
+        # self.report() messages to the console after operator runs.  Sometimes
+        # it does, sometimes it doesn't.  Therefore we also include our own
+        # explicit print() statement.
+        self.report({"WARNING"}, f"{msg}: {err_str}")
         print(f"{msg}: {err_str}")
 
 
@@ -240,26 +346,15 @@ class ScriptMonitorOperator(MonitorOperatorBase):
         self._abs_path = self._local_dir / self.path
         self._name = str(self._abs_path)
 
-    def _init_monitor_modules(self) -> Tuple[List[str], List[Path]]:
+    def _get_monitor_paths(
+        self, mod_name: str, mod_paths: List[Path]
+    ) -> List[Path]:
         if self.monitored_packages is not None:
             # If an explicit set of packages to monitor was specified,
             # just use that.
-            return super()._init_monitor_modules()
+            return super()._get_monitor_paths(mod_name, mod_paths)
 
-        monitored_modules: List[str] = []
-        monitored_paths: List[Path] = [self._abs_path]
-
-        for mod_name, module in sys.modules.items():
-            mod_path_str = getattr(module, "__file__", None)
-            if mod_path_str is None:
-                continue
-            mod_path = Path(mod_path_str)
-
-            if self._local_dir in mod_path.parents:
-                monitored_modules.append(mod_name)
-                monitored_paths.append(Path(mod_path))
-
-        return monitored_modules, monitored_paths
+        return [path for path in mod_paths if self._local_dir in path.parents]
 
     def _run(self) -> None:
         global_namespace = {
